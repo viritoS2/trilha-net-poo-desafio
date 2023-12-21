f49487ec61b0429fb627a5a9ff443d6295dd238e
viritoS2 <vitoramorimof@gmail.com>
2023-12-21 17:47:51 -0300

    alterando modificadores de acesso de protegidos para privados

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,9 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        protected string modelo;
-        protected string imei;
-        protected int memoria;
+        private string modelo;
+        private string imei;
+        private int memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
