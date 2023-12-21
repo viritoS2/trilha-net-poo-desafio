d75e839f50a6fc9233b6c5c133b44b3e89125116
viritoS2 <vitoramorimof@gmail.com>
2023-12-21 16:52:26 -0300

    adicionando propriedades da classe Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,6 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string MyProperty;
+        private string Imei;
+        private int Memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero)
