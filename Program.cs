e952d90fd5a259d6c24da33277f2d88b917fff2b
viritoS2 <vitoramorimof@gmail.com>
2023-12-21 18:35:12 -0300

    adicionando metodos que não estavam sendo utilizados

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111111", memoria: 64);
 nokia.Ligar();
+nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Whatsapp");
 
 Console.WriteLine("\n");
@@ -10,4 +11,5 @@ Console.WriteLine("\n");
 Console.WriteLine("Smartphone iPhone");
 Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "2222222222", memoria: 128);
 iphone.Ligar();
+iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Telegram");
